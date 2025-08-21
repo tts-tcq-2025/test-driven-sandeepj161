ac8221b7e8b3965b87d9a9f1fc0f53cdb4c95302
Sandeep J <j.sandeep.private@gmail.com>
2025-08-21 09:55:04 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,19 +1,28 @@
-#ifndef STRING_CALCULATOR_H
-#define STRING_CALCULATOR_H
+#ifndef STRINGCALCULATOR_H_
+#define STRINGCALCULATOR_H_
 
 #include <string>
 #include <vector>
 #include <stdexcept>
 
 class StringCalculator {
-public:
-    int add(const std::string& input);
+ public:
+  int add(const std::string& input);
 
-private:
-    std::string getDelimiter(const std::string& input);
-    std::string getNumbersPart(const std::string& input, const std::string& delimiter);
-    int sumNumbers(const std::string& numbers, const std::string& delimiter);
-    std::vector<std::string> split(const std::string& str, const std::string& delimiter);
+ private:
+  std::string getDelimiter(const std::string& input);
+  std::string getNumbersPart(const std::string& input,
+                             const std::string& delimiter);
+  std::string extractNumbersAfterPrefix(const std::string& input);
+  std::string replaceNewlinesWithDelimiter(const std::string& input,
+                                           const std::string& delimiter);
+  int sumNumbers(const std::string& numbers, const std::string& delimiter);
+  int parseNumber(const std::string& part);
+  int collectNegativesAndSum(const std::vector<std::string>& parts,
+                             std::vector<int>& negatives);
+  std::string buildNegativesErrorMessage(const std::vector<int>& negatives);
+  std::vector<std::string> split(const std::string& str,
+                                 const std::string& delimiter);
 };
 
 #endif
