ae57a147506e8fe057e02324cd15ff8fe260c529
Sandeep J <j.sandeep.private@gmail.com>
2025-08-27 22:00:04 +0530

    Update string_calculator.h

--- string_calculator.h
+++ string_calculator.h
@@ -3,8 +3,6 @@
 
 #include <stdexcept>
 #include <string>
-#include <utility>
-#include <vector>
 
 class NegativeNumberException : public std::runtime_error {
  public:
@@ -14,24 +12,8 @@ class NegativeNumberException : public std::runtime_error {
 
 class StringCalculator {
  public:
+  // Mirrors Python: add(str_input) -> int
   int add(const std::string& input);
-
- private:
-  // Header parsing
-  bool hasCustomDelimiter(const std::string& input) const;
-  std::pair<std::string, std::string> splitHeaderAndNumbers(
-      const std::string& input) const;
-
-  // Delimiters
-  std::vector<std::string> extractDelimiters(const std::string& header) const;
-
-  // Tokenization and parsing
-  std::vector<std::string> tokenize(
-      const std::string& payload, const std::vector<std::string>& delimiters) const;
-  std::vector<int> parseNumbers(const std::vector<std::string>& tokens) const;
-
-  // Rules
-  void validateNoNegatives(const std::vector<int>& numbers) const;
 };
 
 #endif  // STRING_CALCULATOR_H_
